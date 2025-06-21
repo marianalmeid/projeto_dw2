79c5c6bcdadb3e29e6ad7d5335f4bd07a4739d2d
Mariana <163425141+marianalmeid@users.noreply.github.com>
2025-06-20 22:21:40 -0300

    inicio

---
+++ controle-mensal/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+
+const App = () => {
+  const [despesas, setDespesas] = useState([]);
+  const [form, setForm] = useState({
+    nome: '',
+    categoria: 'fixa',
+    valor: '',
+    data: ''
+  });
+
+
+return (
+  <div>
+    <h1>Controle de Gatsos mensais</h1>
+    
+
+    <h2>Despesas</h2>
+    <table>
+     <tread>
+       <tr>
+        <th>Nome</th>
+        <th>Categoria</th>
+        <th>Valor</th>
+        <th>Data</th>
+      </tr>
+     </tread>
+    </table>
+
+  </div>
+);
+};
+export default App;
